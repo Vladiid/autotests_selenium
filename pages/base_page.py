edca58b2726854836342309bd088b580a9f83a17
Vladiid <vladiid84@gmail.com>
2022-09-20 22:29:10 +0300

    updated base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -30,3 +30,7 @@ class BasePage:
 
     def go_to_element(self, element):
         self.driver.execute_script('arguments[0].scrollIntoView();', element)
+
+    def action_double_click(self):
+
+    def action_right_click(self):
